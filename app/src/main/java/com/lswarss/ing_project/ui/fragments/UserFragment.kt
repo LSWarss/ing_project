ed7956906d98283717d92872aa177ed09a4652f5
LSWarss <ls.warss98@gmail.com>
2020-04-26 17:20:42 +0200

    Corrected map view, but still has some flows close #7

--- app/src/main/java/com/lswarss/ing_project/ui/fragments/UserFragment.kt
+++ app/src/main/java/com/lswarss/ing_project/ui/fragments/UserFragment.kt
@@ -1,11 +1,13 @@
 package com.lswarss.ing_project.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdate
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,7 +22,7 @@ import kotlinx.android.synthetic.main.user_fragment.*
 class UserFragment : Fragment(), OnMapReadyCallback{
 
     private lateinit var googleMap: GoogleMap
-
+    private lateinit var userWithItem: UserWithItem
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,12 +33,13 @@ class UserFragment : Fragment(), OnMapReadyCallback{
         val binding = UserFragmentBinding.inflate(inflater)
         binding.setLifecycleOwner(this)
 
-        val userWithItem = UserFragmentArgs.fromBundle(arguments!!).postProperties
+        userWithItem = UserFragmentArgs.fromBundle(arguments!!).postProperties
 
         val viewModelFactory = UserViewModelFactory(userWithItem, application)
         binding.viewModel = ViewModelProvider(this, viewModelFactory).get(UserViewModel::class.java)
 
-
+        Log.d("latitude", "${userWithItem.user.address.geo.lat.toDouble()}")
+        Log.d("longitude","${userWithItem.user.address.geo.lng.toDouble()}" )
         return binding.root
 
     }
@@ -52,10 +55,17 @@ class UserFragment : Fragment(), OnMapReadyCallback{
         map?.let{
             googleMap = it
         }
-        val user_geo = LatLng(50.2652, 19.0177)
+        val user_geo = LatLng(userWithItem.user.address.geo.lng.toDouble(), userWithItem.user.address.geo.lat.toDouble())
+        Log.d("latitude-onMapReady","${userWithItem.user.address.geo.lat.toDouble()}" )
+        Log.d("longitude-onMapReady","${userWithItem.user.address.geo.lng.toDouble()}" )
         map?.addMarker(MarkerOptions().position(user_geo)
             .title("user_location"))
         map?.moveCamera(CameraUpdateFactory.newLatLng(user_geo))
+        map?.moveCamera(CameraUpdateFactory.zoomTo(3F))
+        map?.uiSettings?.isMyLocationButtonEnabled = false
+        map?.uiSettings?.isZoomGesturesEnabled = false
+        map?.uiSettings?.isScrollGesturesEnabled = false
+        map?.uiSettings?.isTiltGesturesEnabled = false
     }
 
 }
